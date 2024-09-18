48b9a76bc52ca265232df1bcf0985e0c78a7f03c
BenzSumanting <45060446+BenzSumanting@users.noreply.github.com>
2024-09-18 09:35:20 +0800

    first commit

---
+++ app/Http/Controllers/SongController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Song;
+use App\Http\Resources\SongResource;
+use App\Http\Requests\SongRequest;
+
+class SongController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $songs = Song::paginate();
+
+        return response()->json([
+            'data' => SongResource::collection($songs),
+            'meta' => [
+                'current_page' => $songs->currentPage(),
+                'last_page' => $songs->lastPage(),
+                'per_page' => $songs->perPage(),
+                'total' => $songs->total(),
+            ]
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(SongRequest $request)
+    {
+        $new_song = Song::create($request->validated());
+        return (new SongResource($new_song))->response()->setStatusCode(200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Song $song)
+    {
+        return (new SongResource($song))->response()->setStatusCode(200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(SongRequest $request, Song $song)
+    {
+        $song->update( $request->validated() );
+		return (new SongResource($song))->response()->setStatusCode(200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Song $song)
+    {
+        $song->delete();
+		return response()->noContent();
+    }
+}
